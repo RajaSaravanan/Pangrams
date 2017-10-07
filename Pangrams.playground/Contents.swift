c91878903bdfc4f2a65acb8a4de0a6d6f1e8cf73
Raja S <saravanan.raja@konicaminolta.com>
2017-10-07 22:39:26 +0800

    Initial commit

---
+++ Pangrams.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+func isPangram(_ str : String) -> String{
+    let pangram = "abcdefghijklmnopqrstuvwxyz"
+    var input = str
+    input = input.replacingOccurrences(of: " ", with: "")
+    input = input.lowercased()
+    var atLeastOnce = ""
+    for char in input{
+        if (pangram.contains(char) && !atLeastOnce.contains(char)){
+            atLeastOnce += String(char)
+            if (atLeastOnce.count == 26){
+                return "Pangram"
+            }
+        }
+    }
+    return "Not Pangram"
+}
+_ = isPangram("")
+
